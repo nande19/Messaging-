bb3ddaf083b2677c6dc1ee43852d7d158b92a0b1
ST10082757 <ST10082757@vcconnect.edu.za>
2024-12-09 23:03:46 +0200

    Add project files.

---
+++ MSMQDemo/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace MSMQDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessageToSend = new System.Windows.Forms.TextBox();
+            this.txtMessageReceived = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMessageToSend
+            // 
+            this.txtMessageToSend.Location = new System.Drawing.Point(31, 31);
+            this.txtMessageToSend.Multiline = true;
+            this.txtMessageToSend.Name = "txtMessageToSend";
+            this.txtMessageToSend.Size = new System.Drawing.Size(726, 113);
+            this.txtMessageToSend.TabIndex = 0;
+            // 
+            // txtMessageReceived
+            // 
+            this.txtMessageReceived.Location = new System.Drawing.Point(31, 150);
+            this.txtMessageReceived.Multiline = true;
+            this.txtMessageReceived.Name = "txtMessageReceived";
+            this.txtMessageReceived.ReadOnly = true;
+            this.txtMessageReceived.Size = new System.Drawing.Size(734, 118);
+            this.txtMessageReceived.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(94, 296);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 65);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "send queue data";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(618, 296);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 65);
+            this.btnRead.TabIndex = 5;
+            this.btnRead.Text = "Read MSQ";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessageReceived);
+            this.Controls.Add(this.txtMessageToSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessageToSend;
+        private System.Windows.Forms.TextBox txtMessageReceived;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnRead;
+    }
+}
+
---
+++ MSMQDemo/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Messaging;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+
+namespace MSMQDemo
+{
+    public partial class Form1 : Form
+    {
+        // Declare a MessageQueue instance to handle messaging
+        private MessageQueue myMessageQueue = new MessageQueue();
+
+        // Define a UserID variable, initially set to 0
+        private int UserID = 0;
+
+        //--------------------------------------------------------------------------------------------------------//
+
+        /// <summary>
+        /// Constructor of the Form1 class
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set the path of the message queue using UserID
+            this.myMessageQueue.Path = @".\private$\messages" + this.UserID;
+
+            // Set the message formatter to use XML formatter
+            this.myMessageQueue.Formatter = new XmlMessageFormatter(
+            new Type[] { typeof(string) });
+        }
+
+        //--------------------------------------------------------------------------------------------------------//
+
+        /// <summary>
+        /// Event handler for the button click event to send data to the queue
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {            
+            // Check if the message queue path exists
+            if (MessageQueue.Exists(this.myMessageQueue.Path))
+            {
+                // If queue path exists, send data to the queue
+                this.SendData2Queue();
+            }
+            else
+            {
+                // If queue path doesn't exist, create a new queue and send data
+                MessageQueue.Create(this.myMessageQueue.Path);
+                this.SendData2Queue();
+            }
+        }
+        //--------------------------------------------------------------------------------------------------------//
+
+        /// <summary>
+        /// Event handler for the button click event to read data from the queue
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Check if the message queue path exists
+                if (MessageQueue.Exists(this.myMessageQueue.Path))
+                {
+                    // If queue path exists and there are messages in the queue, read the message
+                    if (this.Count(this.myMessageQueue)>0)
+                    {
+                        var myMessage = this.myMessageQueue.Receive();
+                        this.txtMessageReceived.Text = myMessage.Body.ToString();
+                        return;
+                    }
+                }
+                // If queue path doesn't exist or there are no messages, show a message box
+                this.Count(this.myMessageQueue);
+                MessageBox.Show("No Message");
+            }
+            catch (Exception ex) 
+            { 
+            MessageBox.Show(ex.ToString());
+            }
+        }
+        //--------------------------------------------------------------------------------------------------------//
+
+        /// <summary>
+        ///  Method to send data to the queue
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SendData2Queue()
+        {
+            try
+            {
+                // Send the message with text from txtMessageToSend, an alien emoji, and timestamp
+                this.myMessageQueue.Send(this.txtMessageToSend.Text.Trim() + J3QQ4.Emoji.Alien + "\r\n" +
+                    DateTime.Now.ToString());
+                this.myMessageQueue.Close();
+                this.Text = "Message Count:" + this.Count(this.myMessageQueue);
+
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                this.myMessageQueue.Close();
+            }
+        }
+        //--------------------------------------------------------------------------------------------------------//
+
+        /// <summary>
+        /// Method to count the number of messages in the queue
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public int Count(MessageQueue queue)
+        {
+            int count = 0;
+            var emunerator = queue.GetMessageEnumerator();
+
+            while(emunerator.MoveNext())
+            { 
+              count++;
+            }
+
+            this.Text = "Total Messages in Queue" + count; 
+            return count;
+        }
+    }
+}
+        //---------------------------------------- END OF FILE -------------------------------------------------------//
